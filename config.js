058f4ecc762da0db148e32c81bdfbcd9b16cc52e
Und3rC33j <chris.rhodes@outlook.com>
2026-08-19 17:47:22 +0100

    wip: Filey holiday config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://zymihravoahvaivoaxni.supabase.co",
   supabaseAnonKey: "sb_publishable_mKhF52RDq4rFF8gvmu3JqQ_HH1mLOX7",
-  holidayStartDate: "2026-08-10",
-  activeHoliday: "center-parcs-2026",
+holidayStartDate: "2026-08-29",
+activeHoliday: "filey-2026",
+  adminPin: "0912"
 };
